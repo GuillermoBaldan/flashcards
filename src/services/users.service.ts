8b15c4ac1ea212123515c2a8c06b1f664c55c0ef
PaoloSQ <ben281909@gmail.com>
2025-03-23 23:08:12 +0100

    Feat: Añadir validación de contraseña al crear un usuario

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -4,12 +4,23 @@ import { Model } from 'mongoose';
 import { User, UserDocument } from '../modules/users/entities/user.entity';
 import { PasswordHelper } from 'src/helpers/password.helper';
 import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
+import { ERROR_MESSAGES } from 'src/errors/error-messages';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
+  private validatePassword(password: string): void {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      throw new BadRequestException(ERROR_MESSAGES.PASSWORD_WEAK.message);
+    }
+  }
+
   async create(createUserDto: CreateUserDto): Promise<void> {
+    this.validatePassword(createUserDto.password);
+
     const existingUser = await this.userModel
       .findOne({
         $or: [
@@ -21,10 +32,14 @@ export class UsersService {
 
     if (existingUser) {
       if (existingUser.email === createUserDto.email) {
-        throw new BadRequestException('Email already exists');
+        throw new BadRequestException(
+          ERROR_MESSAGES.EMAIL_ALREADY_EXISTS.message,
+        );
       }
       if (existingUser.username === createUserDto.username) {
-        throw new BadRequestException('Username already exists');
+        throw new BadRequestException(
+          ERROR_MESSAGES.USERNAME_ALREADY_EXISTS.message,
+        );
       }
     }
 
@@ -53,7 +68,7 @@ export class UsersService {
   async updateUsername(id: string, username: string): Promise<User> {
     const existingUser = await this.userModel.findById(id).exec();
     if (!existingUser) {
-      throw new BadRequestException('User not found');
+      throw new BadRequestException(ERROR_MESSAGES.USER_NOT_FOUND.message);
     }
     existingUser.username = username;
     return existingUser.save();
@@ -63,7 +78,7 @@ export class UsersService {
     const hashedPassword = await PasswordHelper.hashPassword(password);
     const existingUser = await this.userModel.findById(id).exec();
     if (!existingUser) {
-      throw new BadRequestException('User not found');
+      throw new BadRequestException(ERROR_MESSAGES.USER_NOT_FOUND.message);
     }
     existingUser.password = hashedPassword;
     return existingUser.save();
@@ -72,11 +87,13 @@ export class UsersService {
   async updateEmail(id: string, email: string): Promise<User> {
     const existingUser = await this.userModel.findOne({ email }).exec();
     if (existingUser && existingUser._id.toString() !== id) {
-      throw new BadRequestException('Email already exists');
+      throw new BadRequestException(
+        ERROR_MESSAGES.EMAIL_ALREADY_EXISTS.message,
+      );
     }
     const userToUpdate = await this.userModel.findById(id).exec();
     if (!userToUpdate) {
-      throw new BadRequestException('User not found');
+      throw new BadRequestException(ERROR_MESSAGES.USER_NOT_FOUND.message);
     }
     userToUpdate.email = email;
     return userToUpdate.save();
