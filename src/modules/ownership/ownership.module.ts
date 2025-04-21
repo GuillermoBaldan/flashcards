7a32dfe0e11bbaf73858bbd5f74d21cb8fc8ef04
PaoloSQ <ben281909@gmail.com>
2025-04-21 14:27:24 +0200

    Feat: Desacoplar y crear un servicio que se encargue de verificar la propiedad de un mazo o carta y el usuario solicitante

---
+++ src/modules/ownership/ownership.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OwnershipService } from '../../services/ownership.service';
+import { Deck, DeckSchema } from '../decks/entities/deck.entity';
+import { Card, CardSchema } from '../cards/entities/cards.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Deck.name, schema: DeckSchema },
+      { name: Card.name, schema: CardSchema }
+    ])
+  ],
+  providers: [OwnershipService],
+  exports: [OwnershipService]
+})
+export class OwnershipModule {} 
---
+++ src/services/ownership.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ERROR_MESSAGES } from '../errors/error-messages';
+import { Deck, DeckDocument } from '../modules/decks/entities/deck.entity';
+import { Card, CardDocument } from '../modules/cards/entities/cards.entity';
+
+@Injectable()
+export class OwnershipService {
+  constructor(
+    @InjectModel(Deck.name) private readonly deckModel: Model<DeckDocument>,
+    @InjectModel(Card.name) private readonly cardModel: Model<CardDocument>
+  ) {}
+
+  async verifyDeckOwnership(deckId: string, userId: string): Promise<void> {
+    const deck = await this.deckModel.findOne({ _id: deckId, userId }).exec();
+    if (!deck) {
+      throw new NotFoundException(ERROR_MESSAGES.DECK_NOT_FOUND.message);
+    }
+  }
+
+  async verifyCardOwnership(cardId: string, userId: string): Promise<void> {
+    const card = await this.cardModel.findById(cardId).exec();
+    if (!card) {
+      throw new NotFoundException(ERROR_MESSAGES.CARD_NOT_FOUND.message);
+    }
+
+    const deck = await this.deckModel.findOne({
+      _id: card.deckId,
+      userId
+    }).exec();
+
+    if (!deck) {
+      throw new BadRequestException(ERROR_MESSAGES.UNAUTHORIZED_CARD_ACCESS.message);
+    }
+  }
+}  
