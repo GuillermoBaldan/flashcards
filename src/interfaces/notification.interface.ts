6b39817281dee90e1ba5247bb13c6d5b34a3dfd1
PaoloSQ <ben281909@gmail.com>
2025-05-12 01:32:21 +0200

    Feat: Mejorar interfaces de notificaciones

--- src/interfaces/notification.interface.ts
+++ src/interfaces/notification.interface.ts
@@ -1,7 +1,10 @@
+import { title } from 'process';
 export interface NotificationPayload {
   user_id: string;
-  type: 'deck_share_request' | 'deck_share_response' | 'reminder';
+  type: 'deck_share_request' | 'deck_share_response' | 'reminder' | 'system';
+  title?: string;
   message?: string;
+  action?: string;
   status: 'pending' | 'accepted' | 'rejected' | 'expired' | 'read' | 'archived';
   sender_id?: string;
   metadata?: {
@@ -11,11 +14,15 @@ export interface NotificationPayload {
     deck_name?: string;
     deck_color?: string;
     sender_name?: string;
+    receiver_name?: string;
+    username?: string;
   };
   expires_at?: Date;
   created_at: Date;
+  updated_at?: Date;
 }
 
 // Tipos auxiliares
 export type NotificationType = NotificationPayload['type'];
 export type NotificationStatus = NotificationPayload['status'];
+export type NotificationResponse = NotificationPayload['metadata']['response'];
